f90f9a5eb51dc46fe169b2298ee0d06a54517d99
hminq <hminh250104@gmail.com>
2025-09-16 01:21:31 +0700

    Add Login, Stat, and Top Fragments

---
+++ app/src/main/java/com/hminq/statsfu/presentation/stat/StatFragment.java
@@ -0,0 +1,28 @@
+package com.hminq.statsfu.presentation.stat;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hminq.statsfu.R;
+
+public class StatFragment extends Fragment {
+
+    public StatFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stat, container, false);
+    }
+}
