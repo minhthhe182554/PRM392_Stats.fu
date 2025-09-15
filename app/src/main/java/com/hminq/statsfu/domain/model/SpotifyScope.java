59a1af551e7456b46f5dd5ba8ba25a97d31e0103
hminq <hminh250104@gmail.com>
2025-09-16 01:23:35 +0700

    Add Spotify scopes

---
+++ app/src/main/java/com/hminq/statsfu/domain/model/SpotifyScope.java
@@ -0,0 +1,11 @@
+package com.hminq.statsfu.domain.model;
+
+public class SpotifyScope {
+    public static final String READ_USER_CURRENTLY_PLAYING_SONG = "user-read-currently-playing";
+    public static final String READ_USER_FOLLOWING = "user-follow-read";
+    public static final String READ_USER_TOPS = "user-top-read";
+    public static final String READ_USER_RECENTLY_PLAYED_SONGS = "user-read-recently-played";
+    public static final String READ_USER_LIBRARY = "user-library-read";
+    public static final String READ_USER_EMAIL = "user-read-email";
+    public static final String READ_USER_PRIVATE_DETAILS = "user-read-private";
+}
