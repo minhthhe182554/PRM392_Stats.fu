171e6b900676c01ee0cc27816bd07d1177fe38af
hminq <hminh250104@gmail.com>
2025-09-16 01:25:50 +0700

    feat: add HomeFragment and its layout

---
+++ app/src/main/java/com/hminq/statsfu/presentation/home/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.hminq.statsfu.presentation.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hminq.statsfu.R;
+
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
