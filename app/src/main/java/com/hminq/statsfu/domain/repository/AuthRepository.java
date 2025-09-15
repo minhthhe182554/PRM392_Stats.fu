8f0f521e950b4122438e894c9bb49013feade514
hminq <hminh250104@gmail.com>
2025-09-16 01:24:16 +0700

    Add AuthRepository interface
    This commit introduces the `AuthRepository` interface, which defines a method for exchanging an authorization code for a Spotify access token.

---
+++ app/src/main/java/com/hminq/statsfu/domain/repository/AuthRepository.java
@@ -0,0 +1,9 @@
+package com.hminq.statsfu.domain.repository;
+
+import com.hminq.statsfu.domain.model.SpotifyAuthTokenResponse;
+
+import io.reactivex.rxjava3.core.Single;
+
+public interface AuthRepository {
+    Single<SpotifyAuthTokenResponse> exchangeCodeForToken(String code, String codeVerifier, String clientId, String redirectUri);
+}
